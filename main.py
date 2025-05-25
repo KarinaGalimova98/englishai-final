1695e281bbb464da7921b2096286df2feceab67d
KarinaGalimova98 <karina.galimova.98@mail.ru>
2025-05-25 18:31:04 +0300

    Fix main_route

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager
 from auth import auth_blueprint
 from models import db, User
 from dashboard import dashboard_blueprint
-
+import os
 
 
 
@@ -50,4 +50,5 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=12)
 app.config['SESSION_PERMANENT'] = True
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
