410116cc418c54d2c05b84bcea77add120d762b4
KarinaGalimova98 <karina.galimova.98@mail.ru>
2025-05-26 01:37:40 +0300

    reused openrouter

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager
 from auth import auth_blueprint
 from models import db, User
 from dashboard import dashboard_blueprint
-import os
+
 
 
 
@@ -50,5 +50,4 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=12)
 app.config['SESSION_PERMANENT'] = True
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
