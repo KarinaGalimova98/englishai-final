5cc17a632b62ea1174c5e8839f3528ae258ec3dd
KarinaGalimova98 <karina.galimova.98@mail.ru>
2025-05-28 16:17:32 +0300

    jsdonssdasffваffавапаfdаdffdfd

--- taskgen.py
+++ taskgen.py
@@ -141,7 +141,7 @@ def call_gemini(prompt: str, api_key: str) -> str:
     }
     data = {
         #"model": "google/gemini-2.5-flash-preview-05-20", 
-        "model":"deepseek/deepseek-chat-v3-0324:free",
+        "model":"dgoogle/gemini-2.5-flash-preview-05-20",
         "messages": [
             {"role": "user", "content": prompt}
         ],
